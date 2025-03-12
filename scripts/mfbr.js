c226c6620eaa144ff94cd7bbd66a34e6949ab932
Marco Pavan <marcopavan.mp@gmail.com>
2025-03-12 16:42:22 +0100

    Added w/h to HP images + fix empty stocks in MF ranking page

--- scripts/mfbr.js
+++ scripts/mfbr.js
@@ -10,21 +10,24 @@ function printData (stocks, target) {
   const $detection = $root.querySelector(target + ' ~ p.detection em')
   for (let i = 0; i < stocks.length; i++) {
     const stock = stocks[i]
-    if (i === 0) $detection.innerText = new Date(stock.milFin_mfRanking.now).toLocaleString()
-    const $tr = document.createElement('tr')
-    const $td1 = document.createElement('td')
-    const $a = document.createElement('a')
-    $a.innerText = stock.name
-    $a.title = stock.name
-    $a.href = getStockHref(stock.name, stock.isin)
-    $td1.appendChild($a)
-    $tr.appendChild($td1)
+    if(stock.milFin_mfRanking?.value && stock.milFin_mfRanking?.value !== 'N/A') {
+      if (i === 0) $detection.innerText = new Date(stock.milFin_mfRanking.now).toLocaleString()
+      const $tr = document.createElement('tr')
+      const $td1 = document.createElement('td')
+      const $a = document.createElement('a')
+      $a.innerText = stock.name
+      $a.title = stock.name
+      $a.href = getStockHref(stock.name, stock.isin)
+      $td1.appendChild($a)
+      $tr.appendChild($td1)
 
-    const $td2 = document.createElement('td')
-    $td2.innerText = stock.milFin_mfRanking?.value
-    $tr.appendChild($td2)
+      const $td2 = document.createElement('td')
+      $td2.innerText = stock.milFin_mfRanking?.value
+      $tr.appendChild($td2)
 
-    $tbody.appendChild($tr)
+      $tbody.appendChild($tr)
+    }
+
   }
 }
 
