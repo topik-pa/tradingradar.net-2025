b4e318e4a48ac775e5923951d55a8c860cf7278c
Marco Pavan <marcopavan.mp@gmail.com>
2025-05-15 16:33:53 +0200

    Ava Spot Layer added

---
+++ components/shared/spot_layer/spot_layer.js
@@ -0,0 +1,58 @@
+const spotLayer = {
+  init: () => {
+    console.log('dfdffdfd')
+    const cookieName = 'tradingradarnet-ava-layer'
+    const cookieDuration = 10
+    const cookieLayerWait = 12000
+
+    const cookieLayerElement = document.querySelector('#spot_layer')
+    const avaBannerCookieSet = cookieLayerElement.querySelector('#ava_banner_cookie_set')
+
+    function getCookie (cname) {
+      const name = cname + '='
+      const ca = document.cookie.split(';')
+      for (let i = 0; i < ca.length; i++) {
+        let c = ca[i]
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1)
+        }
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length)
+        }
+      }
+      return ''
+    };
+    function setCookie (cname, cvalue, exdays) {
+      const d = new Date()
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+      const expires = 'expires=' + d.toUTCString()
+      document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
+    }
+    function startCountDownVisibility () {
+      let counter = 20
+      const $counter = document.querySelector('#counter')
+      setInterval(() => {
+        counter--
+        $counter.innerText = counter
+        if(counter<=0) {
+          cookieLayerElement.classList.remove('visible')
+        }
+      }, 1000)
+    }
+
+    if (getCookie(cookieName) === '') {
+      setTimeout(() => {
+        cookieLayerElement.classList.add('visible')
+        startCountDownVisibility()
+      }, cookieLayerWait)
+
+    }
+
+    avaBannerCookieSet.addEventListener('click', () => {
+      setCookie(cookieName, 'true', cookieDuration)
+      cookieLayerElement.classList.remove('visible')
+    })
+  }
+}
+
+export default spotLayer
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,10 @@
 import mainMenu from '../components/shared/header/main-menu/main-menu.js'
 import cookieLayer from '../components/shared/cookie_layer/cookie_layer.js'
+import spotLayer from '../components/shared/spot_layer/spot_layer.js'
 import gotoTop from '../components/shared/goto_top/goto_top.js'
 
 mainMenu.toggleMobileMenu()
 cookieLayer.init()
+spotLayer.init()
 gotoTop.init()
 
